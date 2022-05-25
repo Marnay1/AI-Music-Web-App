8fc9aa158eaa05fd3e529793a3f4d6c51913fc68
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-05-25 19:02:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1= "";
 song2= "";
+leftWristX= 0;
+leftWristY= 0;
+rightWristX= 0;
+rightWristY= 0;
+
 function preload(){
     song1= loadSound("music.mp3");
     song2= loadSound("music2.mp3");
@@ -9,6 +14,21 @@ function setup(){
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
+    poseNet= ml5.poseNet(video, gotResults);
+    poseNet.on("pose", gotPoses);
+}
+function gotPoses(results){
+    if(results > 0){
+        console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("Right Wrist X: " + rightWristX + "Right Wrist Y: " + rightWristY + "Left Wrist X: " + leftWristX + "Left Wrist Y: " + leftWristY);
+    }
+}
+function gotResults(){
+    console.log("Model Loaded");
 }
 function draw(){
     image(video, 0, 0, 600, 500);
