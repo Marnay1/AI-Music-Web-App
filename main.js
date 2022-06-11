410d2ab15af3081ad065be00aa404102a5ed7414
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-06-10 21:27:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ leftWristX= 0;
 leftWristY= 0;
 rightWristX= 0;
 rightWristY= 0;
-
+scoreLeftWrist= 0;
+scoreRightWrist= 0;
+status= "";
 function preload(){
     song1= loadSound("music.mp3");
     song2= loadSound("music2.mp3");
@@ -14,12 +16,15 @@ function setup(){
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
-    poseNet= ml5.poseNet(video, gotResults);
+    poseNet= ml5.poseNet(video, modelLoaded);
     poseNet.on("pose", gotPoses);
 }
 function gotPoses(results){
-    if(results > 0){
+    if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist= results[0].pose.keypoints[10].score; 
+
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
         rightWristX= results[0].pose.rightWrist.x;
@@ -27,9 +32,30 @@ function gotPoses(results){
         console.log("Right Wrist X: " + rightWristX + "Right Wrist Y: " + rightWristY + "Left Wrist X: " + leftWristX + "Left Wrist Y: " + leftWristY);
     }
 }
-function gotResults(){
+function modelLoaded(){
     console.log("Model Loaded");
 }
 function draw(){
     image(video, 0, 0, 600, 500);
+    song1.isPlaying();
+    status= "song1";
+    fill("red");
+    stroke("blue");
+    if (scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if (status == "false"){
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Angry Birds is playing now!";
+        }
+    }
+    status= "song2";
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+        song2.stop();
+        if(status == "false"){
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Thunder is playing now!";
+        }
+    }
 }
